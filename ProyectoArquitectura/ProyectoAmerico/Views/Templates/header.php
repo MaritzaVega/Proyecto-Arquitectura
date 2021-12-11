3f8754276c6302a10aef4a71d9ff90a8ffc1fc01
Angely Lozano <angelylozanodavila1699@gmail.com>
2021-12-11 16:22:30 -0500

    changes

--- ProyectoArquitectura/ProyectoAmerico/Views/Templates/header.php
+++ ProyectoArquitectura/ProyectoAmerico/Views/Templates/header.php
@@ -18,7 +18,7 @@
             <!-- Navbar Search-->
             <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
                 <div class="input-group">
-                    <input class="form-control" type="text" placeholder="chSear for..." aria-label="Search" aria-describedby="basic-addon2" />
+                    <input class="form-control" type="text" placeholder="chSear for A..." aria-label="Search" aria-describedby="basic-addon2" />
                     <div class="input-group-append">
                         <button class="btn btn-primary" type="button"><i class="fas fa-search"></i></button>
                     </div>

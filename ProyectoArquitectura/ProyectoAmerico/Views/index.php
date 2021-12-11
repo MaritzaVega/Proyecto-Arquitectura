46329457ace5bd316eba1da394b3bdf56f5520eb
Angely Lozano <angelylozanodavila1699@gmail.com>
2021-12-11 15:22:27 -0500

    changes

--- ProyectoArquitectura/ProyectoAmerico/Views/index.php
+++ ProyectoArquitectura/ProyectoAmerico/Views/index.php
@@ -20,7 +20,7 @@
                             <div class="col-lg-5">
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
                                   
-                                    <img src="Assets/img/logo.JPG">
+                                    <img src="Assets/img/logo.jpg">
                                     <div class="card-body">
                                         <form id="frmLogin">
                                             <div class="form-group">

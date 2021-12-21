2bf16d98dcb52a239f17656dccae845f7ef28e15
Joselyn Anaya <2016200016@untels.edu.pe>
2021-12-21 16:29:13 -0500

    ultimo

--- ProyectoArquitectura/ProyectoAmerico/Views/Templates/footer.php
+++ ProyectoArquitectura/ProyectoAmerico/Views/Templates/footer.php
@@ -17,21 +17,12 @@
         <script src="<?php echo base_url; ?>Assets/js/jquery-3.6.0.min.js" crossorigin="anonymous"></script>
         <script src="<?php echo base_url; ?>Assets/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="<?php echo base_url; ?>Assets/js/scripts.js"></script>
-<<<<<<< HEAD
-         <!--  <script src="<?php echo base_url; ?>Assets/js/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="<?php echo base_url; ?>Assets/demo/chart-area-demo.js"></script>
-        <script src="<?php echo base_url; ?>Assets/demo/chart-bar-demo.js"></script>!-->
-        <script src="<?php echo base_url; ?>Assets/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
-        <script src="<?php echo base_url; ?>Assets/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
-        <script src="<?php echo base_url; ?>Assets/demo/datatables-demo.js"></script>
-=======
 
         <script src="<?php echo base_url; ?>Assets/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
         <script src="<?php echo base_url; ?>Assets/DataTables/datatables.min.js" crossorigin="anonymous"></script>
        
         <!--https://datatables.net/download/-!-->
 
->>>>>>> 3256519afa0ac33f10b0c5b6078270c44fcd8da3
         <script>
             const base_url = "<?php echo base_url; ?>";
         </script>

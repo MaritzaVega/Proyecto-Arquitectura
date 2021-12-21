44b37b4bef4fe436550cd66ca02ed898a3827b26
Maritza Vega <2016200251@untels.edu.pe>
2021-12-20 19:53:47 -0500

    Generar pdf de compra parte 2 listo - 20/12/2021 - 19:55

--- ProyectoArquitectura/ProyectoAmerico/Views/Compras/reporte.php
+++ ProyectoArquitectura/ProyectoAmerico/Views/Compras/reporte.php
@@ -3,7 +3,7 @@
     <li class="breadcrumb-item active">Nueva Compra</li>
 </ol>
 <table class="table table-light" id="t_reporte_c">
-    <thead class="thead-dark">
+    <thead class="thead-darkR">
         <tr>
             <th>#</th>
             <th>Total</th>

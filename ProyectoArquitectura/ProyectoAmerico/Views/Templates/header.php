954bb9530bd8ecd22190a7e195d2c563e923376c
Angely Lozano <angelylozanodavila1699@gmail.com>
2021-12-26 00:44:55 -0500

    changes

--- ProyectoArquitectura/ProyectoAmerico/Views/Templates/header.php
+++ ProyectoArquitectura/ProyectoAmerico/Views/Templates/header.php
@@ -31,7 +31,9 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
-                        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#cambiarPass">Perfil</a>
+                        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#cambiarPerfil">Perfil</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#cambiarPass">Cambiar Contraseña</a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="<?php echo base_url; ?>Usuarios/salir">Cerrar Sesión</a> <!-- Método salir-->
                     </div>

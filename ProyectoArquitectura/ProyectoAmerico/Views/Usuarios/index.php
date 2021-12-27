a1cf987279511ddb9ff30c7b82d8e3553ea9baa4
Maritza Vega <2016200251@untels.edu.pe>
2021-12-26 22:22:25 -0500

    Cambios jalados de carla

--- ProyectoArquitectura/ProyectoAmerico/Views/Usuarios/index.php
+++ ProyectoArquitectura/ProyectoAmerico/Views/Usuarios/index.php
@@ -63,7 +63,7 @@
                     </div>
                     <div class="form-group">
                         <label for="numDocumento">Número de Documento</label>
-                        <input id="numDocumento" class="form-control" type="number" maxlength="12" name="numDocumento" placeholder="N° Documento" onkeydown="if(this.value.length == 12) return false;">
+                        <input id="numDocumento" class="form-control" type="number" name="numDocumento" placeholder="N° Documento">
                     </div>
                     <button class="btn btn-primary" type="button" onclick="registrarUser(event);" id="btnAccion">Registrar</button>
                     <button class="btn btn-danger" type="button" data-dismiss="modal">Cancelar</button>
